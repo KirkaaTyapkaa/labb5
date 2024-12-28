6b6913f6f83197cdf9fe320108e8283eab30bcd0
Kyrylo Medar <medarkiril@gmail.com>
2024-12-29 00:47:33 +0200

    Почав писати тести для TDD, в мене можна буде додавати та видаляти робітників, також дізнатись про них інформацію

---
+++ src/test/test/department/DepartmentTest.java
@@ -0,0 +1,44 @@
+package department;
+
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DepartmentTest {
+
+    @Test
+    void addEmployee() {
+        Employee employee = new Employee("John", 1000);
+        Department department = new Department("IT");
+        department.addEmployee(employee);
+
+        assertEquals(1, department.getEmployees().size());
+        assertTrue(department.getEmployees().contains(employee));
+    }
+
+    @Test
+    void removeEmployee() {
+        Employee employee = new Employee("John", 1000);
+        Department department = new Department("IT");
+
+        department.addEmployee(employee);
+        department.removeEmployee(employee);
+
+        assertEquals(0, department.getEmployees().size());
+    }
+
+    @Test
+    void getEmployees() {
+        Employee employee = new Employee("John", 1000);
+        Department department = new Department("IT");
+        department.addEmployee(employee);
+
+        List<Employee> result = new ArrayList<>();
+        result.add(employee);
+
+        assertEquals(result, department.getEmployees());
+    }
+
+}
