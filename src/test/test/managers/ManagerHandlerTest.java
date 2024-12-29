032b4af2b9e812c03d91d88113b0382bcc712fda
Kyrylo Medar <medarkiril@gmail.com>
2024-12-29 02:22:42 +0200

    Написав тести для менеджера що видає відпустки, одразу негативний та позитивний тести

---
+++ src/test/test/managers/ManagerHandlerTest.java
@@ -0,0 +1,35 @@
+package managers;
+
+import department.Employee;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ManagerHandlerTest {
+    @Test
+    void positiveHandleRequest() {
+        Employee alice = new Employee("Alice", 5000);
+
+        Request vacationRequest = new Request(alice, RequestType.VACATION);
+        ManagerHandler manager = new ManagerHandler();
+
+        String result = String.format(
+                "Manager approved vacation request for %s",
+                alice.getName()
+        );
+
+        assertEquals(result, manager.handleRequest(vacationRequest));
+    }
+
+    @Test
+    void negativeHandleRequest() {
+        Employee alice = new Employee("Alice", 5000);
+
+        Request vacationRequest = new Request(alice, RequestType.PROMOTION);
+        ManagerHandler manager = new ManagerHandler();
+
+        String result = "Request could not be handled.";
+
+        assertEquals(result, manager.handleRequest(vacationRequest));
+    }
+}
